c89ecc2273e6da48847796700d8bbf2b76e60595
tesujiro <tesujiro@gmail.com>
2019-05-21 21:16:12 +0900

    add: ex1.13

---
+++ ch01/ex1.13/main.go
@@ -0,0 +1,69 @@
+// Server1 is a minimal "echo" server.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}}
+
+const (
+	blackIndex = iota
+	greenIndex
+)
+
+func main() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		c := 5
+		if err := r.ParseForm(); err != nil {
+			log.Print(err)
+		}
+		for k, v := range r.Form {
+			if k == "cycle" {
+				i, err := strconv.Atoi(v[0])
+				if err != nil {
+					log.Print(err)
+					break
+				}
+				c = i
+			}
+		}
+		lissajous(w, c)
+	}
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func lissajous(out io.Writer, c int) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(c)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				greenIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+}
