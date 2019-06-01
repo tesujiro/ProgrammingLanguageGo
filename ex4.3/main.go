3967b7b80a28f4095a8dd4c50731c2fd7241c115
tesujiro <tesujiro@gmail.com>
2019-06-02 07:44:49 +0900

    refact: no variable copy

--- ex4.3/main.go
+++ ex4.3/main.go
@@ -9,10 +9,9 @@ func reverse(s []int) {
 	}
 }
 
-func reverse2(ps *[]int) {
-	s := *ps
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
+func reverse2(s *[]int) {
+	for i, j := 0, len(*s)-1; i < j; i, j = i+1, j-1 {
+		(*s)[i], (*s)[j] = (*s)[j], (*s)[i]
 	}
 }
 
