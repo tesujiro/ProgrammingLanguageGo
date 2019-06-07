1c9ad01e90c9d6010a719cf290bd2577b474baec
tesujiro <tesujiro@gmail.com>
2019-06-08 07:46:01 +0900

    add: open new issue

--- ex4.11/github/editIssue.go
+++ ex4.11/github/editIssue.go
@@ -4,13 +4,29 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 )
 
 func CreateIssue(owner, repo, number string) (*Issue, error) {
-	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
+	issue := EditIssue{Title: "gopl exercise 4.11"}
+	if err := issue.Edit(); err != nil {
+		log.Fatal(err)
+	}
+	/*
+		var jsonStr = []byte(`
+			{
+				"title":"Buy cheese and bread for breakfast.",
+				"labels": ["gopl"]
+			}`)
+	*/
+	jsonStr, err := json.Marshal(issue)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("json: %s\n", jsonStr)
 	buf := bytes.NewBuffer(jsonStr)
 	/*
 		encoder := json.NewEncoder(buf)
@@ -37,10 +53,11 @@ func CreateIssue(owner, repo, number string) (*Issue, error) {
 	if resp.StatusCode >= 300 {
 		return nil, fmt.Errorf("failed to edit issue: %s", resp.Status)
 	}
-	var issue Issue
+	//var issue Issue
 	if err = json.NewDecoder(resp.Body).Decode(&issue); err != nil {
 		return nil, err
 	}
-	return &issue, nil
+	is := Issue{Title: issue.Title, State: issue.State, Body: issue.Body}
+	return &is, nil
 
 }
--- ex4.11/github/github.go
+++ ex4.11/github/github.go
@@ -1,7 +1,15 @@
 // Package github provides a Go API for the GitHub issue tracker.
 package github
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+)
 
 const IssuesURL = "https://api.github.com/search/issues"
 const APIURL = "https://api.github.com"
@@ -19,6 +27,81 @@ type Issue struct {
 	User      *User
 	CreatedAt time.Time `json:"created_at"`
 	Body      string    // in Markdown format
+}
+
+type EditIssue struct {
+	Title string `json:"title"`
+	State string `json:"state"`
+	Body  string `json:"body"` // in Markdown format
+}
+
+func (edit *EditIssue) Edit() error {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		editor = "vim"
+	}
+	editorPath, err := exec.LookPath(editor)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tempfile, err := ioutil.TempFile("", "issue_crud")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tempfile.Close()
+	defer os.Remove(tempfile.Name())
+
+	encoder := json.NewEncoder(tempfile)
+	/*
+		err = encoder.Encode(map[string]string{
+			"title": edit.Title,
+			"state": edit.State,
+			"body":  edit.Body,
+		})
+	*/
+	err = encoder.Encode(edit)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := &exec.Cmd{
+		Path:   editorPath,
+		Args:   []string{editor, tempfile.Name()},
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Reopen the file
+	tempfile.Close()
+	tempfile, err = os.Open(tempfile.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = tempfile.Seek(0, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = json.NewDecoder(tempfile).Decode(&edit); err != nil {
+		fmt.Println("Decode error")
+		log.Fatal(err)
+	}
+	/*
+		fields := make(map[string]string)
+		if err = json.NewDecoder(tempfile).Decode(&fields); err != nil {
+			fmt.Println("Decode error")
+			log.Fatal(err)
+		}
+		edit.title=fields["title"]
+		edit.state=fields["state"]
+		edit.body=fields["body"]
+	*/
+	//fmt.Printf("edit to post: %v\n", edit)
+	return nil
 }
 
 type User struct {
